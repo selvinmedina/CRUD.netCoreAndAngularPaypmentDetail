f079d20eb0b163ad04e44189c2523eaa17599874
SelvinMedina <selvinmedina0016@gmail.com>
2019-11-03 01:46:32 -0600

    Add Payment-detail and payment-detail-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './components/payment-details/payment-details.component';
+import { PaymentDetailComponent } from './components/payment-details/payment-detail/payment-detail.component';
+import { PaymentDetailListComponent } from './components/payment-details/payment-detail-list/payment-detail-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailComponent,
+    PaymentDetailListComponent
   ],
   imports: [
     BrowserModule
